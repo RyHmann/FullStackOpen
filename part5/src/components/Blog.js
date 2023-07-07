dc11025cd3d4868d5897c6fc226585d6c81f8045
Ryan Heitmann <ryan.heitmann@gmail.com>
2023-07-07 16:51:54 +1200

    5.5-5.11

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,7 +1,57 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({blog, likeBlog, deleteBlog}) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const handleLike = () => {
+    likeBlog({
+      title: blog.title,
+      author: blog.author,
+      url: blog.url,
+      likes: blog.likes + 1,
+      id: blog.id
+    })
+  }
+
+  const handleDelete = () => {
+    if (window.confirm(`Remove blog ${blog.title} by ${blog.author}?`)) {
+      try {
+        deleteBlog(blog.id)
+      } catch (error) {
+        console.log(error.message)
+      }
+  }
+}
+
+  return(
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+        {blog.title} {blog.author}<button onClick={toggleVisibility}>view</button>
+      </div>
+      <div style={showWhenVisible}>
+        <div>{blog.title} {blog.author}<button onClick={toggleVisibility}>hide</button></div>
+        <div>{blog.url}</div>
+        <div>{blog.likes} <button onClick={handleLike}>like</button></div>
+        <div>{blog.user.username}</div>
+        <button onClick={handleDelete}>remove</button>
+      </div> 
+   </div> 
+  )
+}
 
 export default Blog
---
+++ part5/src/components/BlogForm.js
@@ -0,0 +1,55 @@
+import { useState } from "react"
+
+const BlogForm = ({createBlog}) => {
+    const [author, setAuthor] = useState('')
+    const [title, setTitle] = useState('')
+    const [url, setUrl] = useState('')
+  
+    const addBlog = (event) => {
+      event.preventDefault()
+      createBlog({
+        title: title,
+        author: author,
+        url: url
+      })
+      setAuthor('')
+      setTitle('')
+      setUrl('')
+    }
+  
+    return (
+      <div>
+        <h1>Add blog</h1>
+        <form onSubmit={addBlog}>
+        <div>
+          title:
+          <input
+          type="text"
+          value={title}
+          name="Title"
+          onChange={ ({target}) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          author:
+          <input
+          type="text"
+          value={author}
+          onChange={ ({target}) => setAuthor(target.value)}
+          />
+        </div>
+        <div>
+          url
+          <input
+          type="text"
+          value={url}
+          onChange={ ({target}) => setUrl(target.value)}
+          />
+        </div>
+        <button type="submit">Add blog</button>
+      </form>
+    </div>
+    )
+  }
+
+export default BlogForm
